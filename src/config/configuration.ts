7f626f5c90f6dda53596dfde466513fd1940559c
Serhii Safonov <137483700+KaratSergio@users.noreply.github.com>
2026-03-25 14:23:00 +0200

    fix ssl configuration

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -9,6 +9,8 @@ export default () => ({
     username: process.env.DB_USERNAME ?? 'postgres',
     password: process.env.DB_PASSWORD ?? 'postgres',
     database: process.env.DB_DATABASE ?? 'marketplace',
+    ssl: process.env.DB_SSL === 'true',
+
   },
 
   jwt: {
